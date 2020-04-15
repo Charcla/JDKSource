9150e1e24a46a437c5f4eac518b6ea1af3be41b4
Charcla <skyou001@163.com>
2020-04-15 22:47:11 +0800

    第一次提交，jdk8的初始代码

---
+++ src/test/Main.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map<String,Integer> map=new HashMap<>();
+
+        map.put("111",1);
+        map.put("222",2);
+        map.put("333",3);
+    }
+}
